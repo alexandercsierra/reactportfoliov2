2ab94db951b3f8e9fdc06f22e4895167644bbf63
Alexander Sierra <alexandercsierra@gmail.com>
2020-03-09 12:37:58 -0400

    added new pw url

--- src/data/projectlist.js
+++ src/data/projectlist.js
@@ -25,7 +25,7 @@ let obj = {
         image: planets,
         desc: "Takes a user's weight on Earth, and planet selection and returns their weight on another planet. The background image will dynamically change according to which planet the user has selected. Build in HTML, CSS and vanilla JavaScript",
         repo: "https://github.com/alexandercsierra/planetary-weights",
-        url: "https://alexandercsierra.github.io/planets/index.html"
+        url: "https://planetary-weights.now.sh/"
     },
     holiday: {
         letter: "H",
