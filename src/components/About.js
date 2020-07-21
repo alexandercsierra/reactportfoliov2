9c9578da06a2967b4f62963ab9b68267640416e5
Alexander Sierra <alexandercsierra@gmail.com>
2020-07-21 14:40:27 -0400

    changed about

--- src/components/About.js
+++ src/components/About.js
@@ -16,7 +16,7 @@ export default function About() {
                 <AboutDiv>
                     <ParaDiv>
                         <Title>About Me</Title>
-                        <Para>Healthcare worker turned web developer, I hold a <Span>B.S. in Health Sciences</Span>. Above all else, I love learning new things, and there is certainly no shortage of that in the development world. My learning is currently taking place at <Span>Lambda School</Span>, where I am also employed as a <Span>Team Lead</Span>.</Para>
+                        <Para>Healthcare worker turned web developer, I hold a <Span>B.S. in Health Sciences</Span>. Above all else, I love learning new things, and there is certainly no shortage of that in the development world. I am currently employed at <Span>Lambda School</Span> as a <Span>Team Lead</Span>.</Para>
 
                         <Para>I find it so satisfying to have a problem, research the solution, then be able to immediately apply my new knowledge to fix a problem I couldn't solve 5 minutes ago. I also dabble in <Span>3D modeling</Span>, having completed a <Span>65+</Span> hour course in <Span>Blender</Span>. When not developing, I can be found playing trumpet in my community band, or frolicking in the snow.</Para>
                     </ParaDiv>
