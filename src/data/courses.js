8396556cd396578243e0aa90e716a37b89afce2f
Alexander Sierra <alexandercsierra@gmail.com>
2020-08-16 23:30:32 -0400

    started typescript

--- src/data/courses.js
+++ src/data/courses.js
@@ -1,61 +1,62 @@
-import lambda from '../imgs/learning/lambda.png'
+import lambda from '../imgs/learning/lambda.png';
 
 export default [
-    {
-        id: 1,
-        title: 'Lambda School',
-        img: lambda,
-        desc: 'A 9-month computer science and software engineering program that provides an immersive hands-on curriculum with a focus on computer science and full-stack web development.',
-        progress: 100,
-        status: 'complete'
-    },
-    {
-        id: 2,
-        title: 'The Coding Interview Bootcamp: Alogrithms + Data Structures',
-        img: 'https://img-a.udemycdn.com/course/240x135/1409142_1879_8.jpg',
-        desc: 'Udemy course',
-        progress: 41,
-        status: 'actively working on'
-    },
-    {
-        id: 3,
-        title: 'Typescript: The Complete Developer\'s Guide [2020]',
-        img: 'https://img-a.udemycdn.com/course/240x135/2337318_abfd_4.jpg',
-        desc: 'Udemy course',
-        progress: 0,
-        status: 'queued for future learning'
-    },
-    {
-        id: 4,
-        title: 'The Modern GraphQL Bootcamp (with Node.js and Apollo)',
-        img: 'https://img-a.udemycdn.com/course/240x135/1720558_d57e.jpg',
-        desc: 'Udemy course',
-        progress: 0,
-        status: 'queued for future learning'
-    },
-    {
-        id: 5,
-        title: 'Microservices with Node JS and React',
-        img: 'https://img-a.udemycdn.com/course/240x135/2887266_c696_5.jpg',
-        desc: 'Udemy course',
-        progress: 0,
-        status: 'queued for future learning'
-    },
-    {
-        id: 6,
-        title: 'The Ultimate React Native Series',
-        img: 'https://process.fs.teachablecdn.com/ADNupMnWyR7kCWRvm76Laz/resize=width:705/https://www.filepicker.io/api/file/4C6yPDywSUeWYLyg1h9G',
-        desc: 'Online course',
-        progress: 0,
-        status: 'queued for future learning'
-    },
-    {
-        id: 7,
-        title: 'Complete Blender Creator: Learn 3D Modelling for Beginners',
-        img: 'https://img-a.udemycdn.com/course/240x135/438522_500f_6.jpg',
-        desc: 'Udemy course',
-        progress: 100,
-        status: 'complete'
-    }
-    
-]
+  {
+    id: 1,
+    title: 'Lambda School',
+    img: lambda,
+    desc:
+      'A 9-month computer science and software engineering program that provides an immersive hands-on curriculum with a focus on computer science and full-stack web development.',
+    progress: 100,
+    status: 'complete',
+  },
+  {
+    id: 2,
+    title: 'The Coding Interview Bootcamp: Alogrithms + Data Structures',
+    img: 'https://img-a.udemycdn.com/course/240x135/1409142_1879_8.jpg',
+    desc: 'Udemy course',
+    progress: 41,
+    status: 'actively working on',
+  },
+  {
+    id: 3,
+    title: "Typescript: The Complete Developer's Guide [2020]",
+    img: 'https://img-a.udemycdn.com/course/240x135/2337318_abfd_4.jpg',
+    desc: 'Udemy course',
+    progress: 1,
+    status: 'actively working on',
+  },
+  {
+    id: 4,
+    title: 'The Modern GraphQL Bootcamp (with Node.js and Apollo)',
+    img: 'https://img-a.udemycdn.com/course/240x135/1720558_d57e.jpg',
+    desc: 'Udemy course',
+    progress: 0,
+    status: 'queued for future learning',
+  },
+  {
+    id: 5,
+    title: 'Microservices with Node JS and React',
+    img: 'https://img-a.udemycdn.com/course/240x135/2887266_c696_5.jpg',
+    desc: 'Udemy course',
+    progress: 0,
+    status: 'queued for future learning',
+  },
+  {
+    id: 6,
+    title: 'The Ultimate React Native Series',
+    img:
+      'https://process.fs.teachablecdn.com/ADNupMnWyR7kCWRvm76Laz/resize=width:705/https://www.filepicker.io/api/file/4C6yPDywSUeWYLyg1h9G',
+    desc: 'Online course',
+    progress: 0,
+    status: 'queued for future learning',
+  },
+  {
+    id: 7,
+    title: 'Complete Blender Creator: Learn 3D Modelling for Beginners',
+    img: 'https://img-a.udemycdn.com/course/240x135/438522_500f_6.jpg',
+    desc: 'Udemy course',
+    progress: 100,
+    status: 'complete',
+  },
+];
