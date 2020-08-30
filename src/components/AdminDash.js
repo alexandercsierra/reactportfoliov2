55607beaa90fdfcd3e9d7c7de98a0f2e33a74bbb
Oliver17! <alexandercsierra@gmail.com>
2020-08-29 21:02:59 -0400

    learning adapted to work with server

--- src/components/AdminDash.js
+++ src/components/AdminDash.js
@@ -17,16 +17,32 @@ const AdminDash = () => {
         priority: 0
     }
 
-    const [project, setProject] = useState(initialProject)
+    const initialCourse = {
+        title:"",
+        img:"",
+        desc:"",
+        progress:0,
+        status:""
+    }
 
-    const handleChange = e => {
+    const [project, setProject] = useState(initialProject)
+    const [course, setCourse] = useState(initialCourse)
+
+    const handleChangeProj = e => {
         setProject({
             ...project,
             [e.target.name]: e.target.value
         })
     }
 
-    const handleSubmit = e => {
+    const handleChangeCourse = e => {
+        setCourse({
+            ...course,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleSubmitProjects = e => {
         e.preventDefault()
         // console.log('project', project)
         axiosWithAuth().post('http://localhost:5001/api/projects', project)
@@ -37,50 +53,87 @@ const AdminDash = () => {
             .catch(err=>console.log(err))
     }
 
+    const handleSubmitCourses = e => {
+        e.preventDefault()
+        console.log('course', course)
+        axiosWithAuth().post('http://localhost:5001/api/courses', course)
+            .then(res=>{
+                console.log(res)
+                setCourse(initialCourse)
+            })
+            .catch(err=>console.log(err))
+    }
+
 
     return(
         <>
             <h1 style={{paddingTop: '15vh'}}>Admin Console</h1>
             <FormContainer>
-                <Form onSubmit={handleSubmit}>
+                <Form onSubmit={handleSubmitProjects}>
                     <h3>Projects</h3>
 
                     <Label htmlFor="title">Title</Label>
-                    <Input name="title" id="title" placeholder="title" onChange={handleChange} value={project.title}/>
+                    <Input name="title" id="title" placeholder="title" onChange={handleChangeProj} value={project.title}/>
 
                     <Label htmlFor="image">Image URL</Label>
-                    <Input name="image" id="image" placeholder="image url" onChange={handleChange} value={project.image}/>
+                    <Input name="image" id="image" placeholder="image url" onChange={handleChangeProj} value={project.image}/>
 
                     <Label htmlFor="alt">Image alt</Label>
-                    <Input name="alt" id="alt" placeholder="alt" onChange={handleChange} value={project.alt}/>
+                    <Input name="alt" id="alt" placeholder="alt" onChange={handleChangeProj} value={project.alt}/>
                     
                     <Label htmlFor="desc">Description</Label>
-                    <Input name="desc" id="desc" placeholder="description" onChange={handleChange} value={project.desc}/>
+                    <Input name="desc" id="desc" placeholder="description" onChange={handleChangeProj} value={project.desc}/>
 
                     <Label htmlFor="repo">Github Repo</Label>
-                    <Input name="repo" id="repo" placeholder="github repo link" onChange={handleChange} value={project.repo}/>
+                    <Input name="repo" id="repo" placeholder="github repo link" onChange={handleChangeProj} value={project.repo}/>
 
                     <Label htmlFor="url">Live Site URL</Label>
-                    <Input name="url" id="url" placeholder="url" onChange={handleChange} value={project.url}/>
+                    <Input name="url" id="url" placeholder="url" onChange={handleChangeProj} value={project.url}/>
 
                     <Label htmlFor="solo">Is this a solo project?</Label>
-                    <Select name="solo" id="solo" onChange={handleChange} value={project.solo}>
+                    <Select name="solo" id="solo" onChange={handleChangeProj} value={project.solo}>
                         <option value={true}>yes</option>
                         <option value={false}>no</option>
                     </Select>
 
                     <Label htmlFor="featured">Is this a featured project?</Label>
-                    <Select name="featured" id="featured" onChange={handleChange} value={project.featured}>
+                    <Select name="featured" id="featured" onChange={handleChangeProj} value={project.featured}>
                         <option value={true}>yes</option>
                         <option value={false}>no</option>
                     </Select>
 
                     <Label htmlFor="priority">Priority</Label>
-                    <Input name="priority" id="priority" type="number" onChange={handleChange} value={project.priority}/>
+                    <Input name="priority" id="priority" type="number" onChange={handleChangeProj} value={project.priority}/>
 
                     <Button type="submit">Submit</Button>
                 </Form>
-                <Form></Form>
+                <Form onSubmit={handleSubmitCourses}>
+                    <h3>Courses</h3>
+
+                    <Label htmlFor="title">Title</Label>
+                    <Input name="title" id="title" placeholder="title" onChange={handleChangeCourse} value={course.title}/>
+
+                    <Label htmlFor="img">Image URL</Label>
+                    <Input name="img" id="img" placeholder="img url" onChange={handleChangeCourse} value={course.img}/>
+
+                    {/* <Label htmlFor="alt">Image alt</Label>
+                    <Input name="alt" id="alt" placeholder="alt" onChange={handleChangeCourse} value={course.alt}/> */}
+                    
+                    <Label htmlFor="desc">Description</Label>
+                    <Input name="desc" id="desc" placeholder="description" onChange={handleChangeCourse} value={course.desc}/>
+
+                    <Label htmlFor="status">Status</Label>
+                    <Select name="status" id="status" onChange={handleChangeCourse} value={course.status}>
+                        <option value="queued for future learning">queued for future learning</option>
+                        <option value="actively working on">actively working on</option>
+                        <option value="complete">complete</option>
+                    </Select>
+
+                    <Label htmlFor="progress">Progress</Label>
+                    <Input name="progress" id="priority" type="number" onChange={handleChangeCourse} value={course.progress}/>
+
+                    <Button type="submit">Submit</Button>
+                </Form>
             </FormContainer>
         </>
     )
@@ -90,18 +143,21 @@ export default AdminDash
 
 const FormContainer = styled.div`
     display: flex;
-    justify-content: center;
-    align-items: center;
+    justify-content: space-around;
+    align-items: flex-start;
+    width: 90%;
+    margin-top: 8%;
 `;
 
 const Form = styled.form`
     display: flex;
     justify-content: center;
-    align-items: center;
+    align-items: flex-start;
     flex-direction: column;
 `;
 const Label = styled.label`
     align-self: flex-start;
+    margin-top: 4%;
 `;
 const Input = styled.input`
     padding: 1.5%;
